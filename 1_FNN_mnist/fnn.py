d886010f2be133f3e07cec4fe107ffc2ca806ccb
AngeloG98 <wenhan.gao98@gmail.com>
2022-01-21 17:28:58 +0800

    one hot encode

--- 1_FNN_mnist/fnn.py
+++ 1_FNN_mnist/fnn.py
@@ -23,8 +23,17 @@ def load_images_labels(imagefile, labelfile):
         image_data = array("B", file.read())
     for i in range(size):
         images.append(np.array(image_data[i * rows * cols:(i + 1) * rows * cols]))
-    dataset = list(zip(images,labels))
+
     return images, labels
+
+def onehot_10(labels):
+    labels_onehot = []
+    for lab in labels:
+        lab_onehot = np.zeros((10,1))
+        lab_onehot[lab] = 1.0
+        labels_onehot.append(lab_onehot)
+    return np.array(labels_onehot)
+
 
 def mnist_process():
     train_imagefile = '0_dataset/MNIST/train-images.idx3-ubyte'
@@ -34,6 +43,9 @@ def mnist_process():
     images, labels = load_images_labels(train_imagefile, train_labelfile)
     test_images, test_labels = load_images_labels(test_imagefile, test_labelfile)
     train_images, valid_images, train_labels, valid_labels = train_test_split(images, labels, test_size = 1/6, random_state = 0)
+    train_labels_onehot = onehot_10(train_labels)
+    train_dataset = list(zip(train_images,train_labels_onehot))
+    print()
 
 if __name__ == "__main__":
     mnist_process()
