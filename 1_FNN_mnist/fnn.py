add normalization fix bug z->z_list

1_FNN_mnist/fnn.py
@@ -46,17 +46,16 @@ class FNN():
         nabla_w = [np.zeros(wi.shape) for wi in self.weights]
         nabla_b = [np.zeros(bi.shape) for bi in self.biases]
         #do back-propagation
-        kkk= self.loss_d(y, a_list[-1])
-        delta = self.loss_d(y, a_list[-1])*self.activate_d(z_list[-1])
+        delta = self.loss_d(y, a_list[-1]) * self.activate_d(z_list[-1])
         nabla_w[-1] = np.dot(delta,a_list[-2].T)
         nabla_b[-1] = delta
         for L in range(2, self.num_layers):
-            delta = np.dot(self.weights[-L+1].T,delta)*self.activate_d(z[-L])
+            delta = np.dot(self.weights[-L+1].T,delta)*self.activate_d(z_list[-L])
             nabla_w[-L] = np.dot(delta,a_list[-L-1].T)
             nabla_b[-L] = delta
         return nabla_w, nabla_b
 
-    def update(self, mini_batch, batchsize, alpha):
+    def update(self, mini_batch, alpha):
         nabla_w = [np.zeros(wi.shape) for wi in self.weights]
         nabla_b = [np.zeros(bi.shape) for bi in self.biases]
         for x,y in mini_batch:
@@ -64,12 +63,12 @@ class FNN():
             nabla_w = [nw+nw_i for nw,nw_i in zip(nabla_w, nabla_w_i)]
             nabla_b = [nb+nb_i for nb,nb_i in zip(nabla_b, nabla_b_i)]
         #gradient descent
-        self.weights = [w - alpha*nw/batchsize for w,nw in zip(self.weights, nabla_w)]
-        self.biases = [b - alpha*nb/batchsize for b,nb in zip(self.biases, nabla_b)]
+        self.weights = [w - alpha/len(mini_batch) *nw for w,nw in zip(self.weights, nabla_w)]
+        self.biases = [b - alpha/len(mini_batch) *nb for b,nb in zip(self.biases, nabla_b)]
         loss_i = 0
         for x,y in mini_batch:
             loss_i += self.loss(y, self.feed_forward(x))
-        self.loss_list.append(loss_i/batchsize)
+        self.loss_list.append(loss_i/len(mini_batch))
         
     def train(self, traning_data, alpha, batchsize, epochs, test_data):
         data_len = len(traning_data)
@@ -78,16 +77,11 @@ class FNN():
             mini_batches = [ traning_data[k:k + batchsize] for k in range(0, data_len, batchsize)]
             k = 0
             for mini_batch in mini_batches:
-                self.update(mini_batch, batchsize, alpha)
+                self.update(mini_batch, alpha)
                 if k%50 == 0:
                     print("epoch:{}, mini_batch:{}, loss:{}, accuracy rate:{}".format(i,k,self.loss_list[-1],self.predict(test_data)/len(test_data)))
                 k += 1
                 
-
     def predict(self, test_data):
         test_results = [(np.argmax(self.feed_forward(x)), y) for (x, y) in test_data]
         return sum(int(x == y) for (x, y) in test_results)
-    
-
-
-    

1_FNN_mnist/mnist.py
@@ -21,7 +21,8 @@ def load_images_labels(imagefile, labelfile):
             raise ValueError('Magic number mismatch, expected 2051, got {}'.format(magic))
         image_data = array("B", file.read())
     for i in range(size):
-        images.append(np.array(image_data[i * rows * cols:(i + 1) * rows * cols]).reshape(rows * cols,1))
+        #Normalization : /255 very important!
+        images.append(np.array(image_data[i * rows * cols:(i + 1) * rows * cols]).reshape(rows * cols,1)/255) 
     return images, labels
 
 def onehot_10(labels):