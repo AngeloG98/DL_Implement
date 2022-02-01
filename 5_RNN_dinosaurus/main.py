ab26efea5b6a74f1dbb9e92b5ee18ee1865735f1
AngeloG98 <wenhan.gao98@gmail.com>
2022-02-01 22:47:48 +0800

    updata backprop

--- 5_RNN_dinosaurus/main.py
+++ 5_RNN_dinosaurus/main.py
@@ -6,4 +6,4 @@ if __name__ == "__main__":
     hidden_size = 100
     in_out_size = len(char_to_ix)
     rnn = RNN(100, in_out_size, in_out_size)
-    rnn.train(train_words, ix_to_char, char_to_ix, batchsize=32, lr=0.1, epochs=30)
+    rnn.train(train_words, ix_to_char, char_to_ix)
--- 5_RNN_dinosaurus/rnn.py
+++ 5_RNN_dinosaurus/rnn.py
@@ -57,8 +57,8 @@ class RNN:
         self.dW_hy = np.zeros(self.W_hy.shape)
         self.db_h = np.zeros(self.b_h.shape)
         self.db_y = np.zeros(self.b_y.shape)
-        #dhnext_h = np.zeros(self.hidden_init.shape)
-        dL_ht = np.zeros(self.hidden_init.shape)
+        # dL_ht = np.zeros(self.hidden_init.shape)
+        dhnext = np.zeros(self.hidden_init.shape)
 
         for t in reversed(range(len(input))):
             # backprop through layer
@@ -66,21 +66,35 @@ class RNN:
             dLt_yt[target[t]] -= 1
             self.dW_hy += np.dot(dLt_yt, self.h_seq[t].T)
             self.db_y += dLt_yt
-            # backprop through time
-            dLt_ht = np.dot(self.W_hy.T, dLt_yt)
-            if t == len(input)-1:
-                dL_ht = dLt_ht
-            else:
-                dhnext_h = np.dot(self.W_hh.T, (1 - self.h_seq[t+1] * self.h_seq[t+1]))
-                dL_ht = dLt_ht + dhnext_h * dL_ht
-            dL_zt = (1 - self.h_seq[t] * self.h_seq[t]) * dL_ht
-            self.dW_hh += np.dot(dL_zt, self.h_seq[t].T)
-            self.dW_xh += np.dot(dL_zt, self.x_seq[t].T)
-            self.db_h += dL_zt
+
+            # backprop through time 1
+            # dLt_ht = np.dot(self.W_hy.T, dLt_yt)
+            # if t == len(input)-1:
+            #     dL_ht = dLt_ht
+            # else:
+            #     dhnext_h = np.dot(self.W_hh.T, (1 - self.h_seq[t+1] * self.h_seq[t+1]))
+            #     dL_ht = dLt_ht + dhnext_h * dL_ht
+            # dL_zt = (1 - self.h_seq[t] * self.h_seq[t]) * dL_ht
+            # self.dW_hh += np.dot(dL_zt, self.h_seq[t].T)
+            # self.dW_xh += np.dot(dL_zt, self.x_seq[t].T)
+            # self.db_h += dL_zt
+
+            # backprop through time 2
+            dh = np.dot(self.W_hy.T, dLt_yt) + dhnext # backprop into h. 
+            dhraw = (1 - self.h_seq[t] * self.h_seq[t]) * dh # backprop through tanh nonlinearity #tanh'(x) = 1-tanh^2(x)
+            self.db_h += dhraw
+            self.dW_xh += np.dot(dhraw, self.x_seq[t].T)
+            self.dW_hh += np.dot(dhraw, self.h_seq[t-1].T)
+            dhnext = np.dot(self.W_hh.T, dhraw)
         for dparam in [self.dW_xh, self.dW_hh, self.dW_hy, self.db_h, self.db_y]: 
             np.clip(dparam, -5, 5, out=dparam) # gradient clip
 
-    def train(self, train_data, ix_to_char, char_to_ix, batchsize=32, lr=0.1, epochs=10):
+    def train(self, train_data, ix_to_char, char_to_ix, batchsize=2, lr=0.1, epochs=30):
+        mW_xh = np.zeros(self.W_xh.shape)
+        mW_hh = np.zeros(self.W_hh.shape)
+        mW_hy = np.zeros(self.W_hy.shape)
+        mb_h = np.zeros(self.b_h.shape)
+        mb_y = np.zeros(self.b_y.shape)
         for epoch in range(epochs):
             np.random.shuffle(train_data)
             for i in range(0, len(train_data), batchsize):
@@ -99,12 +113,52 @@ class RNN:
                     self.db_y_b += self.db_y
 
                 k = len(batch_data)
-                for param, dparam in zip([self.W_xh, self.W_hh, self.W_hy, self.b_h, self.b_y],
-                                        [self.dW_xh_b/k, self.dW_hh_b/k, self.dW_hy_b/k, self.db_h_b/k, self.db_y_b/k]):
-                    param += -lr * dparam
+                for param, dparam,mem in zip([self.W_xh, self.W_hh, self.W_hy, self.b_h, self.b_y],
+                                        [self.dW_xh_b, self.dW_hh_b, self.dW_hy_b, self.db_h_b, self.db_y_b],
+                                        [mW_xh, mW_hh, mW_hy, mb_h, mb_y]):
+                    mem += dparam * dparam
+                    param += -lr/k * dparam / np.sqrt(mem + 1e-8)
             
-                if i%128 == 0:
-                    print("epoch: {}, batch: {}, batchsize: {}, loss: {:.4f}".format(epoch, i, k, self.loss))
+            # print each epoch        
+            print("epoch: {}, loss: {:.4f}".format(epoch, self.loss))
+            seed = 0
+            for i in range(5):
+                print("predict: ", self.predict(char_to_ix, ix_to_char, seed)[1])
+                seed += 1
 
         
+    def predict(self, char_to_ix, ix_to_char, seed):
+        x = np.zeros((self.in_size, 1))
+        h_prev = np.zeros(self.hidden_init.shape)
+        
+        indices = []
+        idx = -1 
+        counter = 0
+        newline_character = char_to_ix['\n']
+        
+        while (idx != newline_character and counter != 50):
+            # forward
+            h = np.tanh(np.dot(self.W_xh, x) + np.dot(self.W_hh, h_prev) + self.b_h)
+            y = self.softmax(np.dot(self.W_hy, h) + self.b_y)
+            # sample
+            np.random.seed(counter + seed) 
+            idx = np.random.choice(range(len(y)),p=y.ravel())
+            indices.append(idx)
+            # update x h
+            x = np.zeros((self.in_size, 1))
+            x[idx] = 1
+            h_prev = h
+            # update seed
+            seed += 1
+            counter +=1
+        if (counter == 50):
+            indices.append(char_to_ix['\n'])
+        
+        indices_char = ''
+        for i in indices:
+            indices_char += ix_to_char[i]
+
+        return indices, indices_char[:-1]
+
+    
 
